88e9fd99c4d89a1d695927866539d1136f7abe57
Nikita Zimin <n.zimin@aristos.pw>
2024-06-17 14:27:39 +0300

    Module 9 Lesson 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello React!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 }
 
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import style from './Main.module.css';
+import Layout from '../Layout';
+
+export const Main = () => {
+  return (
+    <main className={style.main}>
+      <Layout>Body</Layout>
+    </main>
+  );
+};
