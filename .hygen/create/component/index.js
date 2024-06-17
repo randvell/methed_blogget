acec015226ff047da6bddb81ea58a414e08ef043
Nikita Zimin <n.zimin@aristos.pw>
2024-06-17 23:44:53 +0300

    Module 9 Lesson 3

--- .hygen/create/component/index.js
+++ .hygen/create/component/index.js
@@ -9,16 +9,15 @@ module.exports = {
       {
         type: 'input',
         name: 'dir',
-        message: 'Вложенная директория?(Optional)'
-      }
-    ]
+        message: 'Вложенная директория?(Optional)',
+      },
+    ];
 
-
-    return inquirer.prompt(questions).then(answers => {
+    return inquirer.prompt(questions).then((answers) => {
       const {component_name, dir} = answers;
       const path = `${dir ? `${dir}/` : ''}${component_name}`;
       const absPath = `src/components/${path}`;
       return {...answers, path, absPath};
     });
-  }
-}
+  },
+};
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,15 @@ import Search from './Search';
 import Auth from './Auth';
 import Heading from './Heading';
 
-export const Header = () => {
-  return (
-    <header className={style.header}>
-      <Layout>
-        <div className={style.gridContainer}>
-          <Logo />
-          <Heading text="Заголовок"/>
-          <Search />
-          <Auth auth={false} />
-        </div>
-      </Layout>
-    </header>
-  );
-};
+export const Header = () => (
+  <header className={style.header}>
+    <Layout>
+      <div className={style.gridContainer}>
+        <Logo />
+        <Heading text="Заголовок" />
+        <Search />
+        <Auth auth={false} />
+      </div>
+    </Layout>
+  </header>
+);
