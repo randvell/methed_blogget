dec395ab01bc3ad98514130b72e9daee160a249c
Nikita Zimin <n.zimin@aristos.pw>
2024-06-20 21:10:16 +0300

    Inline svg component

--- src/UI/InlineSvg/InlineSvg.jsx
+++ src/UI/InlineSvg/InlineSvg.jsx
@@ -1,10 +1,29 @@
+import React, {useEffect, useState} from 'react';
+// import style from './InlineSvg.module.css';
 import PropTypes from 'prop-types';
 
-export const InlineSvg = ({href}) => (
-  <svg>
-    <use href={href} />
-  </svg>
-);
+export const InlineSvg = ({href}) => {
+  const [svg, setSvg] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isErrored, setIsErrored] = useState(false);
+
+  useEffect(() => {
+    fetch(href)
+      .then((res) => res.text())
+      .then(setSvg)
+      .catch(setIsErrored)
+      .then(() => setIsLoaded(true));
+  }, [href]);
+
+  if (isErrored) {
+    return '';
+  }
+  if (!isLoaded) {
+    return '';
+  }
+
+  return <div dangerouslySetInnerHTML={{__html: svg}} />;
+};
 
 InlineSvg.propTypes = {
   href: PropTypes.string,
