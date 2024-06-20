85d3bfc1bc81b417aa2da95bb80241f4e5e3e66e
Nikita Zimin <n.zimin@aristos.pw>
2024-06-20 21:17:23 +0300

    Replaced Svg with React components

--- src/UI/InlineSvg/InlineSvg.jsx
+++ src/UI/InlineSvg/InlineSvg.jsx
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 // import style from './InlineSvg.module.css';
 import PropTypes from 'prop-types';
 
-export const InlineSvg = ({href}) => {
+export const InlineSvg = ({href, height, width}) => {
   const [svg, setSvg] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [isErrored, setIsErrored] = useState(false);
@@ -22,9 +22,17 @@ export const InlineSvg = ({href}) => {
     return '';
   }
 
-  return <div dangerouslySetInnerHTML={{__html: svg}} />;
+  return (
+    <div
+      height={height}
+      width={width}
+      dangerouslySetInnerHTML={{__html: svg}}
+    />
+  );
 };
 
 InlineSvg.propTypes = {
   href: PropTypes.string,
+  height: PropTypes.number,
+  width: PropTypes.number,
 };
--- src/components/Header/Logo/Logo.jsx
+++ src/components/Header/Logo/Logo.jsx
@@ -1,8 +1,8 @@
 import style from './Logo.module.css';
-import logo from './img/logo.svg';
+import {ReactComponent as LogoSvg} from './img/logo.svg';
 
 export const Logo = () => (
   <a className={style.link} href="/">
-    <img className={style.logo} src={logo} alt="Логотип Blogget" />
+    <LogoSvg className={style.logo} alt="Логотип Blogget" />
   </a>
 );
